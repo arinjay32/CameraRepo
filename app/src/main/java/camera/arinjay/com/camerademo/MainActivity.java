b972de54d2a65e75f07b49239b13c1081104b511
Arinjay Patni <arinjay@greenspades.com>
2019-09-22 13:00:04 +0530

    profile margin

--- app/src/main/java/camera/arinjay/com/camerademo/MainActivity.java
+++ app/src/main/java/camera/arinjay/com/camerademo/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //abdajfkap
 
         takePictureButton = (Button) findViewById(R.id.button_image);
         imageView = (ImageView) findViewById(R.id.imageview);
